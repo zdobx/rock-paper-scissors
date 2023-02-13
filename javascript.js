b9527bd86f265fc23e1d69208ac6c85b38ad32d8
zdobx <zdobson@protonmail.com>
2023-02-13 18:24:06 +1000

    Generate random computer answer

--- javascript.js
+++ javascript.js
@@ -1,11 +1,11 @@
-function getComputerChoice () {
-     const choice = ("Rock", "Paper", "Scissors")
-     const randomIndex = Math.floor(Math.random() * words.length);
-     return words(randomIndex);
 
-
-     console.log(words);
+function getComputerChoice () { //computer is supposed to generate random answer.
      
+     const choices = ['Rock', 'Paper', 'Scissors'];
+     const choice = Math.floor(Math.random() * choices.length);
+     return finalChoice = choices[choice];
+
+
 }
 
-
+console.log(getComputerChoice());
