6c99864ec8243d840d0adf1b99ebaa83e3a81462
zdobx <zdobson@protonmail.com>
2023-02-13 18:54:16 +1000

    Added function for paper and scissors

--- javascript.js
+++ javascript.js
@@ -14,16 +14,35 @@ console.log(getComputerChoice());
 function playerSelection () { //prompt player to select rock, paper or scissors.
 
      let selection = prompt("What would you like to choose? Paper, rock or scissors?");
+     
+     //When the computer selects rock
      if (playerSelection === 'rock' && getComputerChoice === 'Rock') {
      return alert("DRAW!") }  
      
      else if (playerSelection === 'paper' && getComputerChoice === 'Rock') {
      return alert("WIN!") }
-
      
      else if (playerSelection === 'scissors' && getComputerChoice === 'Rock') {
      return alert("LOSER!")  }
 
-}
+     //When the computer selects paper
+     if (playerSelection === 'rock' && getComputerChoice === 'Paper') {
+          return alert("LOSER!") }  
+          
+          else if (playerSelection === 'paper' && getComputerChoice === 'Paper') {
+          return alert("DRAW!") }
+          
+          else if (playerSelection === 'scissors' && getComputerChoice === 'Paper') {
+          return alert("WINNER!")  }
+
+}    //When the computer selects scissors
+     if (playerSelection === 'rock' && getComputerChoice === 'Scissors') {
+     return alert("WINNER!") }  
+     
+     else if (playerSelection === 'paper' && getComputerChoice === 'Scissors') {
+     return alert("LOSER!") }
+     
+     else if (playerSelection === 'scissors' && getComputerChoice === 'Scissors') {
+     return alert("DRAW!")  }
 
 prompt(playerSelection())
