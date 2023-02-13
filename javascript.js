08f52100855ab922688965a1366a607be92078da
zdobx <zdobson@protonmail.com>
2023-02-13 18:51:08 +1000

    Added initial playerselection function

--- javascript.js
+++ javascript.js
@@ -9,3 +9,21 @@ function getComputerChoice () { //computer is supposed to generate random answer
 }
 
 console.log(getComputerChoice());
+
+
+function playerSelection () { //prompt player to select rock, paper or scissors.
+
+     let selection = prompt("What would you like to choose? Paper, rock or scissors?");
+     if (playerSelection === 'rock' && getComputerChoice === 'Rock') {
+     return alert("DRAW!") }  
+     
+     else if (playerSelection === 'paper' && getComputerChoice === 'Rock') {
+     return alert("WIN!") }
+
+     
+     else if (playerSelection === 'scissors' && getComputerChoice === 'Rock') {
+     return alert("LOSER!")  }
+
+}
+
+prompt(playerSelection())
